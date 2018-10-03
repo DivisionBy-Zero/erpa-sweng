9da56642e5ed22c8bfd189ee0e30f611b53f3437
Haley Owsianko <haley.owsianko@gmail.com>
2018-10-03 16:37:30 +0200

    Implement launcher for Game List and Create Game activities

--- app/src/main/java/ch/epfl/sweng/erpa/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/erpa/MainActivity.java
@@ -1,13 +1,27 @@
 package ch.epfl.sweng.erpa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launch_game_list(View view)
+    {
+        Intent intent = new Intent(this, GameList.class);
+        startActivity(intent);
+    }
+
+    public void launch_create_game(View view)
+    {
+        Intent intent = new Intent(this, CreateGame.class);
+        startActivity(intent);
     }
 }
