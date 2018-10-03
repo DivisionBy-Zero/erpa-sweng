763b78904e7114b68538a3198a83058b57737588
Haley Owsianko <haley.owsianko@gmail.com>
2018-10-03 16:37:30 +0200

    Implement launcher for Game List and Create Game activities

---
+++ app/src/main/java/ch/epfl/sweng/erpa/CreateGame.java
@@ -0,0 +1,13 @@
+package ch.epfl.sweng.erpa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_game);
+    }
+}
---
+++ app/src/main/java/ch/epfl/sweng/erpa/GameList.java
@@ -0,0 +1,13 @@
+package ch.epfl.sweng.erpa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_list);
+    }
+}
