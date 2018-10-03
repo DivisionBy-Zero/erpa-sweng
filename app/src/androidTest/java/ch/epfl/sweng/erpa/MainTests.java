a57fd6b533b70365f3755874a92f46a238862678
Haley Owsianko <haley.owsianko@gmail.com>
2018-10-03 17:43:20 +0200

    Finish implementing tests

--- app/src/androidTest/java/ch/epfl/sweng/erpa/MainTests.java
+++ app/src/androidTest/java/ch/epfl/sweng/erpa/MainTests.java
@@ -1,7 +1,10 @@
 package ch.epfl.sweng.erpa;
 
+import android.content.ComponentName;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -10,6 +13,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
@@ -17,18 +21,22 @@ import static android.support.test.espresso.intent.matcher.IntentMatchers.hasCom
 @RunWith(AndroidJUnit4.class)
 public class MainTests {
     @Rule
-    public final ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public final IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
 
     @Test
     public void testCanLaunchGameList()
     {
         onView(ViewMatchers.withId(R.id.launch_game_list_button)).perform(ViewActions.click());
-        intended(hasComponent(GameList.class.getName()));
+        //Intents.init();
+        intended(hasComponent(new ComponentName(getTargetContext(), GameList.class)));
+        //Intents.release();
     }
     @Test
     public void testCanLaunchCreateGame()
     {
         onView(ViewMatchers.withId(R.id.launch_create_game_button)).perform(ViewActions.click());
+        //Intents.init();
         intended(hasComponent(CreateGame.class.getName()));
+        //Intents.release();
     }
 }
