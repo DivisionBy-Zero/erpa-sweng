05449f0ef8916213f30477caf8316ce800bdb8a7
Ryker <ariane.ryker@hotmail.com>
2018-10-10 00:13:58 +0200

    add GameAdapter class and dummy Game class [to be deleted]

---
+++ app/src/main/java/ch/epfl/sweng/erpa/model/Game.java
@@ -0,0 +1,13 @@
+package ch.epfl.sweng.erpa.model;
+
+public class Game {
+    public String title;
+    public String location;
+    public String universe;
+
+    public Game() {
+        title = "test";
+        location = "Lausanne";
+        universe = "DnD";
+    }
+}
