0654c656d2c2357b10d69555bbd304135faaecf1
Ryker <ariane.ryker@hotmail.com>
2018-10-10 00:35:52 +0200

    add activity that displays the list of games

--- app/src/main/java/ch/epfl/sweng/erpa/activities/GameListActivity.java
+++ app/src/main/java/ch/epfl/sweng/erpa/activities/GameListActivity.java
@@ -2,14 +2,43 @@ package ch.epfl.sweng.erpa.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 import ch.epfl.sweng.erpa.R;
+import ch.epfl.sweng.erpa.model.Game;
+import ch.epfl.sweng.erpa.model.GameAdapter;
 
 public class GameListActivity extends Activity {
+
+    private ArrayList<Game> games = new ArrayList<>();
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new GameAdapter(this, games);
+        mRecyclerView.setAdapter(mAdapter);
+
+        createListData();
+    }
+
+    private void createListData() {
+        for (int i = 0; i < new Random().nextInt(20) + 5; i++) games.add(new Game());
+        mAdapter.notifyDataSetChanged();
     }
 }
