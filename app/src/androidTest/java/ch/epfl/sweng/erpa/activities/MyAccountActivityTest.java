a87c0f3463930738619570f5ff11b641688dd65c
Anne Donnet <anneviolon@gmail.com>
2018-10-10 16:16:55 +0200

    add button in mainActivity to access to myAccount

--- app/src/androidTest/java/ch/epfl/sweng/erpa/activities/MyAccountActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/erpa/activities/MyAccountActivityTest.java
@@ -18,41 +18,41 @@ import static org.junit.Assert.*;
 
 public class MyAccountActivityTest {
     @Rule
-    public final IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MyAccountActivity.class);
+    public final IntentsTestRule<MyAccountActivity> intentsTestRule = new IntentsTestRule<>(MyAccountActivity.class);
 
     @Test
     public void testCanLaunchPendingRequestActivity() {
-        onView(ViewMatchers.withId(R.id.launch_pending_request_activity_button)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.launch_pending_request_activity_layout)).perform(ViewActions.click());
         intended(hasComponent(new ComponentName(getTargetContext(), PendingRequestActivity.class)));
     }
 
     @Test
     public void testCanLaunchConfirmedGamesActivity() {
-        onView(ViewMatchers.withId(R.id.launch_confirmed_games_activity_button)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.launch_confirmed_games_activity_layout)).perform(ViewActions.click());
         intended(hasComponent(ConfirmedGamesActivity.class.getName()));
     }
 
     @Test
     public void testCanLaunchPastGamesActivity() {
-        onView(ViewMatchers.withId(R.id.launch_past_games_activity_button)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.launch_past_games_activity_layout)).perform(ViewActions.click());
         intended(hasComponent(PastGamesActivity.class.getName()));
     }
 
     @Test
     public void testCanLaunchHostedGamesActivity() {
-        onView(ViewMatchers.withId(R.id.launch_hosted_games_activity_button)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.launch_hosted_games_activity_layout)).perform(ViewActions.click());
         intended(hasComponent(HostedGamesActivity.class.getName()));
     }
 
     @Test
     public void testCanLaunchPastHostedGamesActivity() {
-        onView(ViewMatchers.withId(R.id.launch_past_hosted_games_activity_button)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.launch_past_hosted_games_activity_layout)).perform(ViewActions.click());
         intended(hasComponent(PastHostedGamesActivity.class.getName()));
     }
 
     @Test
     public void testCanLaunchProfileActivity() {
-        onView(ViewMatchers.withId(R.id.launch_profile_activity_button)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.launch_profile_activity_layout)).perform(ViewActions.click());
         intended(hasComponent(ProfileActivity.class.getName()));
     }
 }
