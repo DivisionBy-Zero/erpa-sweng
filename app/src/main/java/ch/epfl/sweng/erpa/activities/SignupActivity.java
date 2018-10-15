c3175e809d63c77c546ccce68243ac8777ff3ed2
Ivan Topcic <ivan.topcic@epfl.ch>
2018-10-15 17:59:00 +0200

    Initial files

---
+++ app/src/main/java/ch/epfl/sweng/erpa/activities/SignupActivity.java
@@ -0,0 +1,15 @@
+package ch.epfl.sweng.erpa.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.epfl.sweng.erpa.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
