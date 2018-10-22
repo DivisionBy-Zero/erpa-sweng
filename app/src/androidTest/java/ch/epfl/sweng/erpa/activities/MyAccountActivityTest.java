604bf2732175d39aafdb66788734361bf3ca9b7a
Anne Donnet <anneviolon@gmail.com>
2018-10-22 08:43:21 +0200

    + tests in MyAccountActivityTest

--- app/src/androidTest/java/ch/epfl/sweng/erpa/activities/MyAccountActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/erpa/activities/MyAccountActivityTest.java
@@ -1,59 +1,137 @@
 package ch.epfl.sweng.erpa.activities;
 
-import android.content.ComponentName;
-import android.support.test.espresso.action.ViewActions;
+import android.app.Instrumentation;
+import android.content.res.Resources;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.espresso.matcher.ViewMatchers;
+import android.widget.ListView;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import ch.epfl.sweng.erpa.R;
+import ch.epfl.sweng.erpa.model.MyAccountButton;
+import ch.epfl.sweng.erpa.model.UserProfile;
 
-import static android.support.test.InstrumentationRegistry.getTargetContext;
-import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class MyAccountActivityTest {
-    @Rule
-    public final IntentsTestRule<MyAccountActivity> intentsTestRule = new IntentsTestRule<>(MyAccountActivity.class);
 
-    // TODO : (Anne) rewrite tests to match with refactoring
-//    @Test
-//    public void testCanLaunchPendingRequestActivity() {
-//        onView(ViewMatchers.withId(R.id.launch_pending_request_activity_layout)).perform(ViewActions.click());
-//        intended(hasComponent(new ComponentName(getTargetContext(), PendingRequestActivity.class)));
-//    }
-//
-//    @Test
-//    public void testCanLaunchConfirmedGamesActivity() {
-//        onView(ViewMatchers.withId(R.id.launch_confirmed_games_activity_layout)).perform(ViewActions.click());
-//        intended(hasComponent(ConfirmedGamesActivity.class.getName()));
-//    }
-//
-//    @Test
-//    public void testCanLaunchPastGamesActivity() {
-//        onView(ViewMatchers.withId(R.id.launch_past_games_activity_layout)).perform(ViewActions.click());
-//        intended(hasComponent(PastGamesActivity.class.getName()));
-//    }
-//
-//    @Test
-//    public void testCanLaunchHostedGamesActivity() {
-//        onView(ViewMatchers.withId(R.id.launch_hosted_games_activity_layout)).perform(ViewActions.click());
-//        intended(hasComponent(HostedGamesActivity.class.getName()));
-//    }
-//
-//    @Test
-//    public void testCanLaunchPastHostedGamesActivity() {
-//        onView(ViewMatchers.withId(R.id.launch_past_hosted_games_activity_layout)).perform(ViewActions.click());
-//        intended(hasComponent(PastHostedGamesActivity.class.getName()));
-//    }
-//
-//    @Test
-//    public void testCanLaunchProfileActivity() {
-//        onView(ViewMatchers.withId(R.id.launch_profile_activity_layout)).perform(ViewActions.click());
-//        intended(hasComponent(ProfileActivity.class.getName()));
-//    }
+    private ListView listView;
+    private Resources systemResources;
+    private UserProfile userProfile;
+    private Instrumentation instrumentation;
+
+    @Rule
+    public final IntentsTestRule<MyAccountActivity> intentsTestRule = new IntentsTestRule<>(
+            MyAccountActivity.class);
+
+    @Before
+    public void setTestVariables() {
+        systemResources = intentsTestRule.getActivity().getResources();
+        listView = intentsTestRule.getActivity().findViewById(R.id.myAccountLayout);
+        userProfile = intentsTestRule.getActivity().userProfile;
+        instrumentation = InstrumentationRegistry.getInstrumentation();
+    }
+
+    @Test
+    public void testFindRightNumberOfButtonsInTheList() {
+        if (userProfile.getIsPlayer()) {
+            if (userProfile.getIsGm())
+                assertThat(listView.getCount(), is(6));
+            else
+                assertThat(listView.getCount(), is(4));
+        } else
+            assertThat(listView.getCount(), is(3));
+    }
+
+    @Test
+    public void testButtonsNamesInTheList() {
+        if (userProfile.getIsPlayer()) {
+            assertThat(((MyAccountButton) listView.getItemAtPosition(0)).getText(),
+                    is(systemResources.getString(R.string.pendingRequestText)));
+            assertThat(((MyAccountButton) listView.getItemAtPosition(1)).getText(),
+                    is(systemResources.getString(R.string.confirmedGamesText)));
+            assertThat(((MyAccountButton) listView.getItemAtPosition(2)).getText(),
+                    is(systemResources.getString(R.string.pastGamesText)));
+            if (userProfile.getIsGm()) {
+                assertThat(((MyAccountButton) listView.getItemAtPosition(3)).getText(),
+                        is(systemResources.getString(R.string.hostedGamesText)));
+                assertThat(((MyAccountButton) listView.getItemAtPosition(4)).getText(),
+                        is(systemResources.getString(R.string.pastHostedGamesText)));
+                assertThat(((MyAccountButton) listView.getItemAtPosition(5)).getText(),
+                        is(systemResources.getString(R.string.profileText)));
+            } else
+                assertThat(((MyAccountButton) listView.getItemAtPosition(3)).getText(),
+                        is(systemResources.getString(R.string.profileText)));
+        } else {
+            assertThat(((MyAccountButton) listView.getItemAtPosition(0)).getText(),
+                    is(systemResources.getString(R.string.hostedGamesText)));
+            assertThat(((MyAccountButton) listView.getItemAtPosition(1)).getText(),
+                    is(systemResources.getString(R.string.pastHostedGamesText)));
+            assertThat(((MyAccountButton) listView.getItemAtPosition(2)).getText(),
+                    is(systemResources.getString(R.string.profileText)));
+        }
+    }
+
+    @Test
+    public void testShouldShowPendingRequestOrHostedGamesWhenFirstButtonClicked() {
+
+        instrumentation.runOnMainSync(() -> {
+            int position = 0;
+            listView.performItemClick(listView.getChildAt(position), position,
+                    listView.getAdapter().getItemId(position));
+        });
+
+        if (userProfile.getIsPlayer())
+            intended(hasComponent(PendingRequestActivity.class.getName()));
+        else
+            intended(hasComponent(HostedGamesActivity.class.getName()));
+    }
+
+    @Test
+    public void testShouldShowConfirmedGamesOrPastHostedGamesWhenSecondButtonClicked() {
+
+        instrumentation.runOnMainSync(() -> {
+            int position = 1;
+            listView.performItemClick(listView.getChildAt(position), position,
+                    listView.getAdapter().getItemId(position));
+        });
+
+        if (userProfile.getIsPlayer())
+            intended(hasComponent(ConfirmedGamesActivity.class.getName()));
+        else
+            intended(hasComponent(PastHostedGamesActivity.class.getName()));
+    }
+
+    @Test
+    public void testShouldShowPastGamesOrProfileWhenThirdButtonClicked() {
+
+        instrumentation.runOnMainSync(() -> {
+            int position = 2;
+            listView.performItemClick(listView.getChildAt(position), position,
+                    listView.getAdapter().getItemId(position));
+        });
+
+        if (userProfile.getIsPlayer())
+            intended(hasComponent(PastGamesActivity.class.getName()));
+        else
+            intended(hasComponent(ProfileActivity.class.getName()));
+    }
+
+    @Test
+    public void testShouldShowProfileWhenLastButtonClicked() {
+
+        instrumentation.runOnMainSync(() -> {
+            int position = listView.getChildCount() - 1;
+            listView.performItemClick(listView.getChildAt(position), position,
+                    listView.getAdapter().getItemId(position));
+        });
+
+        intended(hasComponent(ProfileActivity.class.getName()));
+    }
 }
