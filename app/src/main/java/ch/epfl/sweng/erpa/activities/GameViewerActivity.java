2f4ab0620673efc00884e0b14a94eebd3d635b2f
Haley Owsianko <haley.owsianko@gmail.com>
2018-10-29 19:14:32 +0100

    Implement join game button

--- app/src/main/java/ch/epfl/sweng/erpa/activities/GameViewerActivity.java
+++ app/src/main/java/ch/epfl/sweng/erpa/activities/GameViewerActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.annimon.stream.Optional;
+import com.annimon.stream.Stream;
 
 import java.util.NoSuchElementException;
 
@@ -13,15 +14,21 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ch.epfl.sweng.erpa.R;
 import ch.epfl.sweng.erpa.model.Game;
+import ch.epfl.sweng.erpa.model.UserProfile;
 import ch.epfl.sweng.erpa.services.GameService;
 import ch.epfl.sweng.erpa.services.RemoteServicesProvider;
 
 import static android.content.ContentValues.TAG;
 
 public class GameViewerActivity extends DependencyConfigurationAgnosticActivity {
-    @Inject RemoteServicesProvider rsp;
+    @Inject GameService gs;
+    // TODO(@Sapphie) change this once proper login is implemented
+    @Inject UserProfile up;
+
+    private Game game;
 
     @BindView(R.id.titleTextView) TextView title;
     @BindView(R.id.descriptionTextView) TextView description;
@@ -31,6 +38,7 @@ public class GameViewerActivity extends DependencyConfigurationAgnosticActivity 
     @BindView(R.id.oneShotOrCampaignTextView) TextView type;
     @BindView(R.id.sessionNumberTextView) TextView numSessions;
     @BindView(R.id.sessionLengthTextView) TextView sessionLength;
+    @BindView(R.id.participatingPlayersTextView) TextView playersInGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +50,17 @@ public class GameViewerActivity extends DependencyConfigurationAgnosticActivity 
     @Override
     protected void onResume() {
         super.onResume();
-        Optional<Game> optGame;
-        String gameId = getGameId();
-        optGame = rsp.getGameService().getOne(gameId);
+        updateGame();
+    }
 
+    private void getGameOrFinish(Optional<Game> optGame) {
         if (optGame.isPresent()) {
-            Game game = optGame.get();
-            updateFields(game);
+            game = optGame.get();
         } else {
             Log.d(TAG, "onResume: could not find game in database. Exiting", new NoSuchElementException());
             finish();
         }
     }
-
     private String getGameId() {
 
         String gameId = getIntent().getStringExtra(GameService.PROP_INTENT_GAME);
@@ -68,8 +74,19 @@ public class GameViewerActivity extends DependencyConfigurationAgnosticActivity 
         }
     }
 
+    @OnClick(R.id.joinGameButton)
+    void joinGame() {
+        game = game.withPlayer(up.getUuid());
+        gs.saveGame(game);
+        updateGame();
+    }
+
+    private void updateGame() {
+        getGameOrFinish(gs.getGame(getGameId()));
+        updateFields();
+    }
     @SuppressLint("SetTextI18n")
-    private void updateFields(Game game) {
+    private void updateFields() {
         Log.d(TAG, "onResume: Successfully fetched game");
 
         title.setText(game.getName());
@@ -78,9 +95,15 @@ public class GameViewerActivity extends DependencyConfigurationAgnosticActivity 
         universe.setText(game.getUniverse());
         difficulty.setText(game.getDifficulty().toString());
         type.setText(game.getOneshotOrCampaign().toString());
+
         String numSessionsString = game.getNumberSessions().map(Object::toString).orElse("Unspecified");
         numSessions.setText(numSessionsString);
+
         String gameLength = game.getSessionLengthInMinutes().map(Object::toString).orElse("Unspecified");
         sessionLength.setText(gameLength);
+
+        String playerInfo = Stream.of(game.getPlayersUuid()).reduce("", (elem, acc)->acc + "\n" + elem);
+        playersInGame.setText(playerInfo);
+
     }
 }
