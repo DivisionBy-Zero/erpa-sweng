cf65bdf39331b582d372a8ddaf5528a9abb0b14d
Anne Donnet <anneviolon@gmail.com>
2018-11-27 18:04:28 +0100

    + DiceAnimationActivity
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

---
+++ app/src/main/java/ch/epfl/sweng/erpa/activities/DiceAnimationActivity.java
@@ -0,0 +1,48 @@
+package ch.epfl.sweng.erpa.activities;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+
+import ch.epfl.sweng.erpa.R;
+import ch.epfl.sweng.erpa.activities.sketches.DieSketch;
+import ch.epfl.sweng.erpa.views.FlowLayout;
+import processing.android.CompatUtils;
+import processing.android.PFragment;
+import processing.core.PApplet;
+
+public class DiceAnimationActivity extends AppCompatActivity {
+
+    private FrameLayout frame;
+    private PApplet sketch;
+    private FlowLayout flowLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DataBindingUtil.setContentView(this, R.layout.activity_dice_animation);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        flowLayout = findViewById(R.id.dice_animation_flowLayout);
+
+        addNewDice("objs/d4R.obj", 1.5f);
+        addNewDice("objs/d6R.obj", 0.5f);
+        addNewDice("objs/d8R.obj", 0.5f);
+        addNewDice("objs/d10R.obj", 0.5f);
+        addNewDice("objs/d20_1R.obj", 0.5f);
+    }
+
+    public void addNewDice(String fileName, float factor) {
+        frame = new FrameLayout(this);
+        frame.setId(CompatUtils.getUniqueViewId());
+        flowLayout.addView(frame);
+
+        sketch = new DieSketch(fileName, factor);
+        PFragment fragment = new PFragment(sketch);
+        fragment.setView(frame, this);
+    }
+
+}
