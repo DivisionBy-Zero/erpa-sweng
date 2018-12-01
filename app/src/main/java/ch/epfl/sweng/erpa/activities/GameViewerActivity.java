bbadf1d67b2f67f03f9455f4c515801846e17f2d
Ryker <ariane.benali@epfl.ch>
2018-12-01 20:44:59 +0100

    modify GameViewer to display game info using the expandable listview

--- app/src/main/java/ch/epfl/sweng/erpa/activities/GameViewerActivity.java
+++ app/src/main/java/ch/epfl/sweng/erpa/activities/GameViewerActivity.java
@@ -1,13 +1,20 @@
 package ch.epfl.sweng.erpa.activities;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.annimon.stream.Optional;
-import com.annimon.stream.Stream;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import javax.inject.Inject;
@@ -16,29 +23,31 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import ch.epfl.sweng.erpa.R;
+import ch.epfl.sweng.erpa.activities.GameListActivity.GameList;
+import ch.epfl.sweng.erpa.listeners.ListLikeOnClickListener;
+import ch.epfl.sweng.erpa.model.GameViewerAdapter;
 import ch.epfl.sweng.erpa.model.Game;
+import ch.epfl.sweng.erpa.model.PlayerAdapter;
 import ch.epfl.sweng.erpa.model.UserProfile;
 import ch.epfl.sweng.erpa.services.GameService;
-import ch.epfl.sweng.erpa.services.RemoteServicesProvider;
 
 import static android.content.ContentValues.TAG;
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+import static ch.epfl.sweng.erpa.activities.GameListActivity.GAME_LIST_VIEWER_ACTIVITY_CLASS_KEY;
+import static ch.epfl.sweng.erpa.activities.GameListActivity.GameList.FIND_GAME;
+import static ch.epfl.sweng.erpa.activities.GameListActivity.GameList.HOSTED_GAMES;
+import static ch.epfl.sweng.erpa.activities.GameListActivity.GameList.PENDING_REQUEST;
 
 public class GameViewerActivity extends DependencyConfigurationAgnosticActivity {
     @Inject GameService gs;
     // TODO(@Sapphie) change this once proper login is implemented
     @Inject UserProfile up;
-
+    @BindView(R.id.gameViewerExpandableList) ExpandableListView expListView;
+    @BindView(R.id.joinGameButton) Button joinGameButton;
     private Game game;
-
-    @BindView(R.id.titleTextView) TextView title;
-    @BindView(R.id.descriptionTextView) TextView description;
-    @BindView(R.id.gmTextView) TextView gmName;
-    @BindView(R.id.universeTextView) TextView universe;
-    @BindView(R.id.difficultyTextView) TextView difficulty;
-    @BindView(R.id.oneShotOrCampaignTextView) TextView type;
-    @BindView(R.id.sessionNumberTextView) TextView numSessions;
-    @BindView(R.id.sessionLengthTextView) TextView sessionLength;
-    @BindView(R.id.participatingPlayersTextView) TextView playersInGame;
+    private List<String> mListDataHeader = new ArrayList<>();
+    private HashMap<String, LinearLayout> mListDataChild = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,19 +59,70 @@ public class GameViewerActivity extends DependencyConfigurationAgnosticActivity 
     @Override
     protected void onResume() {
         super.onResume();
+
+        Intent myIntent = getIntent();
+        Bundle bundle = myIntent.getExtras();
+        finishIfNull(bundle);
+
         updateGame();
+
+        GameList gameList = (GameList) bundle.getSerializable(
+                GAME_LIST_VIEWER_ACTIVITY_CLASS_KEY);
+        finishIfNull(gameList);
+        setJoinButton(gameList);
+
+        prepareListData(gameList);
+
+        GameViewerAdapter mListAdapter = new GameViewerAdapter(this, game, mListDataHeader,
+                mListDataChild);
+        expListView.setAdapter(mListAdapter);
+    }
+
+    public void setJoinButton(GameList gameList) {
+        int joinGameButtonVisibility = gameList == FIND_GAME ? VISIBLE : GONE;
+        joinGameButton.setVisibility(joinGameButtonVisibility);
+    }
+
+    private void prepareListData(GameList gameList) {
+        View view = LayoutInflater.from(this).inflate(R.layout.game_viewer_expandable_list_item,
+                null);
+
+        mListDataHeader.add("Game Information");
+        mListDataChild.put(mListDataHeader.get(0),
+                view.findViewById(R.id.gameViewerGameDescription));
+
+        if (!gameList.equals(FIND_GAME) && !gameList.equals(PENDING_REQUEST)) {
+            ArrayList<String> uuidArray = new ArrayList<>(game.getPlayersUuid());
+
+            mListDataHeader.add("List of Players");
+            mListDataChild.put(mListDataHeader.get(1),
+                    view.findViewById(R.id.gameViewerPlayerList));
+
+            ListLikeOnClickListener mListener = ((v, position) -> {
+                game = game.removePlayer(uuidArray.get(position));
+                gs.saveGame(game);
+                uuidArray.remove(position);
+            });
+
+            ListView playerListView = view.findViewById(R.id.gameViewerPlayerListView);
+            PlayerAdapter myPlayerAdapter = new PlayerAdapter(this, uuidArray,
+                    gameList.equals(HOSTED_GAMES),
+                    mListener);
+            playerListView.setAdapter(myPlayerAdapter);
+        }
     }
 
     private void getGameOrFinish(Optional<Game> optGame) {
         if (optGame.isPresent()) {
             game = optGame.get();
         } else {
-            Log.d(TAG, "onResume: could not find game in database. Exiting", new NoSuchElementException());
+            Log.d(TAG, "onResume: could not find game in database. Exiting",
+                    new NoSuchElementException());
             finish();
         }
     }
-    private String getGameId() {
 
+    private String getGameId() {
         String gameId = getIntent().getStringExtra(GameService.PROP_INTENT_GAME);
         if (gameId == null) {
             Exception thrown = new IllegalArgumentException("Game Id not found");
@@ -74,8 +134,8 @@ public class GameViewerActivity extends DependencyConfigurationAgnosticActivity 
         }
     }
 
-    @OnClick(R.id.joinGameButton)
-    void joinGame() {
+    @butterknife.Optional @OnClick(R.id.joinGameButton)
+    public void joinGame() {
         game = game.withPlayer(up.getUuid());
         gs.saveGame(game);
         updateGame();
@@ -83,27 +143,10 @@ public class GameViewerActivity extends DependencyConfigurationAgnosticActivity 
 
     private void updateGame() {
         getGameOrFinish(gs.getGame(getGameId()));
-        updateFields();
     }
-    @SuppressLint("SetTextI18n")
-    private void updateFields() {
-        Log.d(TAG, "onResume: Successfully fetched game");
 
-        title.setText(game.getName());
-        description.setText(game.getDescription());
-        gmName.setText(game.getGmUuid());
-        universe.setText(game.getUniverse());
-        difficulty.setText(game.getDifficulty().toString());
-        type.setText(game.getOneshotOrCampaign().toString());
-
-        String numSessionsString = game.getNumberSessions().map(Object::toString).orElse("Unspecified");
-        numSessions.setText(numSessionsString);
-
-        String gameLength = game.getSessionLengthInMinutes().map(Object::toString).orElse("Unspecified");
-        sessionLength.setText(gameLength);
-
-        String playerInfo = Stream.of(game.getPlayersUuid()).reduce("", (elem, acc)->acc + ", " + elem);
-        playersInGame.setText(playerInfo);
-
+    private void finishIfNull(Object obj) {
+        if (obj == null) finish();
     }
 }
+
