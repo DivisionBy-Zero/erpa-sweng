43cde03c015cfc12cd8522711dcf99b06690a167
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-12-10 21:23:15 +0100

    Activities: Wire and rework UserProfileActivity
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- app/src/main/java/ch/epfl/sweng/erpa/activities/UserProfileActivity.java
+++ app/src/main/java/ch/epfl/sweng/erpa/activities/UserProfileActivity.java
@@ -1,19 +1,11 @@
 package ch.epfl.sweng.erpa.activities;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.annimon.stream.Optional;
+import com.annimon.stream.function.Supplier;
 
 import java.util.NoSuchElementException;
 
@@ -22,70 +14,79 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ch.epfl.sweng.erpa.R;
+import ch.epfl.sweng.erpa.operations.AsyncTaskService;
 import ch.epfl.sweng.erpa.model.UserProfile;
-import ch.epfl.sweng.erpa.services.UserProfileService;
+import ch.epfl.sweng.erpa.operations.OptionalDependencyManager;
+import ch.epfl.sweng.erpa.services.UserManagementService;
 
-import static android.content.ContentValues.TAG;
 import static ch.epfl.sweng.erpa.util.ActivityUtils.addNavigationMenu;
-import static ch.epfl.sweng.erpa.util.ActivityUtils.onNavigationItemMenuSelected;
-import static ch.epfl.sweng.erpa.util.ActivityUtils.setUsernameInMenu;
 
 public class UserProfileActivity extends DependencyConfigurationAgnosticActivity {
+    @Inject OptionalDependencyManager optionalDependency;
+    @Inject UserManagementService ups;
 
-    @Inject UserProfileService ups;
-    @Inject UserProfile up;
+    @BindView(R.id.usernameTextView) TextView usernameTV;
+    @BindView(R.id.experienceTextView) TextView experienceTV;
+    @BindView(R.id.playerOrGMTextView) TextView playerOrGmTV;
 
-    @BindView(R.id.usernameTextView) TextView username;
-    @BindView(R.id.experienceTextView) TextView experience;
-    @BindView(R.id.playerOrGMTextView) TextView playerOrGm;
+    private AsyncTaskService asyncTaskService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (dependenciesNotReady()) return;
         setContentView(R.layout.activity_user_profile);
         ButterKnife.bind(this);
+        asyncTaskService = new AsyncTaskService();
+        asyncTaskService.setResultConsumerContext(this::runOnUiThread);
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
+        if (dependenciesNotReady()) return;
 
-        String uuid = getUuidFromIntent();
-        Optional<UserProfile> optUserProfile = ups.getUserProfile(uuid);
-        if (optUserProfile.isPresent()) {
-            updateFields(optUserProfile.get());
-        } else {
-            Log.d(TAG, "onResume: could not find UserProfile in database. Exiting", new NoSuchElementException());
-            finish();
-        }
+        String userUuid = getUuidFromIntent();
 
-        addNavigationMenu(this, findViewById(R.id.user_profile_drawer_layout), findViewById(R.id.user_profile_navigation_view), up);
+        Supplier<Throwable> notFoundException = () ->
+            new NoSuchElementException("Could not find User Profile with UUID " + userUuid);
 
+        asyncTaskService.run(() -> ups.getUserProfile(userUuid).orElseThrow(notFoundException),
+            this::updateUserProfile, this::asyncDataFetchErrorHandler);
+
+        asyncTaskService.run(() -> ups.getUsernameFromUserUuid(userUuid).orElseThrow(notFoundException),
+            username -> usernameTV.setText("@" + username.getUsername()), this::asyncDataFetchErrorHandler);
+
+        addNavigationMenu(this, findViewById(R.id.user_profile_drawer_layout),
+            findViewById(R.id.user_profile_navigation_view), optionalDependency);
     }
 
-    @SuppressLint("SetTextI18n") private void updateFields(UserProfile userProfile) {
-        username.setText(userProfile.getUsername());
-        experience.setText(userProfile.getXp().toString());
+    @SuppressLint("SetTextI18n") private void updateUserProfile(UserProfile userProfile) {
+        experienceTV.setText(UserProfile.Experience.Casual.toString());
         if (userProfile.getIsGm() && userProfile.getIsPlayer())
-            playerOrGm.setText("Player and Game master");
+            playerOrGmTV.setText(getString(R.string.user_profile_player_and_gm));
         else if (userProfile.getIsPlayer())
-            playerOrGm.setText("Player");
+            playerOrGmTV.setText(getString(R.string.user_profile_player_only));
         else if (userProfile.getIsGm())
-            playerOrGm.setText("Game master");
+            playerOrGmTV.setText(getString(R.string.user_profile_gm_only));
         else
-            playerOrGm.setText("");
+            playerOrGmTV.setText(getString(R.string.user_profile_neither_player_nor_gm));
     }
 
     private String getUuidFromIntent() {
-        String uuid = getIntent().getStringExtra(UserProfileService.PROP_INTENT_USER);
-        if (uuid != null)
-            return uuid;
-        else {
-            RuntimeException thrown = new IllegalArgumentException("User Id not found");
-            Log.d(UserProfileActivity.class.getName(), "no user id passed with intent", thrown);
+        String userUuid = getIntent().getStringExtra(UserManagementService.PROP_INTENT_USER);
+        if (userUuid == null) {
+            RuntimeException thrown = new IllegalArgumentException("UserUuid property not found");
+            Log.e("retrieveUuid", "Cannot show User" , thrown);
             finish();
             throw thrown;
         }
+        return userUuid;
+    }
+
+    private void asyncDataFetchErrorHandler(Throwable exc) {
+        Log.e("asyncErrorHandler", "Cannot populate UserProfileActivity", exc);
+        finish();
     }
 }
