ca6a9ea67489a4c5eaddf8dfcf9c35c1aa791b19
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-12-10 20:31:59 +0100

    + PlayerJoinGameRequest entity
    This entity represents the state of a player within a game.
    
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- app/src/main/java/ch/epfl/sweng/erpa/model/PlayerJoinGameRequest.java
+++ app/src/main/java/ch/epfl/sweng/erpa/model/PlayerJoinGameRequest.java
@@ -12,8 +12,8 @@ import lombok.NonNull;
 @AllArgsConstructor
 public class PlayerJoinGameRequest {
     @NonNull private String joinRequestId;
-    @NonNull private String gameUuid;
     @NonNull private RequestStatus requestStatus;
+    @NonNull private String gameUuid;
     @NonNull private String userUuid;
 
     public enum RequestStatus {REQUEST_TO_JOIN, CONFIRMED, REJECTED, REMOVED, HAS_QUIT}
