c6c0c5431a0ca3b697ecd31343c13615caafd175
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-12-10 21:17:46 +0100

    + JoinGameActivity
    This is a somewhat phantom activity. It's mainly used for dependency resolution and
    execution. We may add an animation later.
    
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

---
+++ app/src/main/java/ch/epfl/sweng/erpa/activities/JoinGameActivity.java
@@ -0,0 +1,50 @@
+package ch.epfl.sweng.erpa.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import javax.inject.Inject;
+
+import ch.epfl.sweng.erpa.R;
+import ch.epfl.sweng.erpa.model.UserSessionToken;
+import ch.epfl.sweng.erpa.operations.AsyncTaskService;
+import ch.epfl.sweng.erpa.services.GameService;
+
+import static ch.epfl.sweng.erpa.util.ActivityUtils.createPopup;
+
+public class JoinGameActivity extends DependencyConfigurationAgnosticActivity {
+    public static final String GAME_UUID_KEY = "game_uuid";
+    @Inject GameService gs;
+    @Inject UserSessionToken sessionToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_game);
+        if (dependenciesNotReady()) return;
+
+        AsyncTaskService asyncTaskService = new AsyncTaskService();
+        asyncTaskService.setResultConsumerContext(this::runOnUiThread);
+
+        String gameUuid = getGameUuidFromIntent(getIntent());
+
+        asyncTaskService.run(() -> gs.joinGame(gameUuid), joinRequest -> {
+            createPopup("Join Request sent!", this, this::finish);
+        }, this::handleException);
+    }
+
+    void handleException(Throwable exc) {
+        createPopup("Could not submit join request: " + exc.getMessage(), this, this::finish);
+    }
+
+    static String getGameUuidFromIntent(Intent intent) {
+        String gameUuid = intent.getStringExtra(GAME_UUID_KEY);
+        if (gameUuid == null) {
+            RuntimeException thrown = new IllegalArgumentException("GameUuid property not found");
+            Log.e("retrieveUuid", "Cannot join Game", thrown);
+            throw thrown;
+        }
+        return gameUuid;
+    }
+}
