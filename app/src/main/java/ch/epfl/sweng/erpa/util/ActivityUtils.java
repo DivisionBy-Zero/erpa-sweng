43e8807227398334f0911ef038463464354ab7e6
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-12-10 20:36:39 +0100

    ActivityUtils: Generify toolbar population and add optional callback to createPopup
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- app/src/main/java/ch/epfl/sweng/erpa/util/ActivityUtils.java
+++ app/src/main/java/ch/epfl/sweng/erpa/util/ActivityUtils.java
@@ -3,7 +3,6 @@ package ch.epfl.sweng.erpa.util;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -13,28 +12,26 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.annimon.stream.Optional;
-
-import javax.inject.Inject;
 
 import ch.epfl.sweng.erpa.R;
 import ch.epfl.sweng.erpa.activities.CreateGameActivity;
 import ch.epfl.sweng.erpa.activities.DiceActivity;
 import ch.epfl.sweng.erpa.activities.GameListActivity;
 import ch.epfl.sweng.erpa.activities.MyAccountActivity;
-import ch.epfl.sweng.erpa.model.UserProfile;
-import ch.epfl.sweng.erpa.services.UserProfileService;
-
-import static android.support.v4.app.ActivityCompat.startActivityForResult;
-import static ch.epfl.sweng.erpa.activities.GameListActivity.GAME_LIST_ACTIVTIY_CLASS_KEY;
+import ch.epfl.sweng.erpa.model.Username;
+import ch.epfl.sweng.erpa.operations.OptionalDependencyManager;
 
 public class ActivityUtils {
-
     public static void createPopup(String text, Context context) {
+        createPopup(text, context, () -> {
+        });
+    }
+
+    public static void createPopup(String text, Context context, Runnable onUserAcknowledge) {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
 
         final TextView tv = new TextView(context);
@@ -46,10 +43,7 @@ public class ActivityUtils {
         alertDialogBuilder.setView(tv);
 
         // set dialog message
-        alertDialogBuilder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-            }
-        });
+        alertDialogBuilder.setCancelable(false).setPositiveButton("OK", (dialog, id) -> onUserAcknowledge.run());
 
         // create alert dialog
         AlertDialog alertDialog = alertDialogBuilder.create();
@@ -58,52 +52,50 @@ public class ActivityUtils {
     }
 
     //Handle navigationMenu interactions
-    public static void addNavigationMenu(Activity activity, DrawerLayout mDrawerLayout, NavigationView navigationView, UserProfile up){
-        navigationView.setNavigationItemSelectedListener(
-                menuItem -> onNavigationItemMenuSelected(menuItem, mDrawerLayout, activity));
-        setUsernameInMenu(navigationView, up);
+    public static void addNavigationMenu(Activity activity, DrawerLayout mDrawerLayout,
+                                         NavigationView navigationView, OptionalDependencyManager optionalDependencyManager) {
+        installDefaultNavigationMenuHandler(navigationView, mDrawerLayout, activity);
+        setUsernameInMenu(navigationView, optionalDependencyManager.get(Username.class));
     }
 
-    public static boolean onNavigationItemMenuSelected(MenuItem menuItem, DrawerLayout mDrawerLayout, Activity activity) {
-        // close drawer when item is tapped
-        mDrawerLayout.closeDrawers();
+    public static void installDefaultNavigationMenuHandler(NavigationView navigationView,
+                                                           DrawerLayout drawerLayout, Context ctx) {
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            drawerLayout.closeDrawers(); // close the drawer where item was tapped
+            Intent intent;
 
-        Bundle bundle = new Bundle();
-
-        Intent intent = new Intent(activity, MyAccountActivity.class);
-        switch (menuItem.getItemId()) {
-            case R.id.menu_createGame:
-                intent = new Intent(activity, CreateGameActivity.class);
-                break;
-            case R.id.menu_findGame:
-                bundle.putSerializable(GAME_LIST_ACTIVTIY_CLASS_KEY, GameListActivity.GameList.FIND_GAME);
-                intent = new Intent(activity, GameListActivity.class);
-                intent.putExtras(bundle);
-                break;
-            case R.id.menu_dice:
-                intent = new Intent(activity, DiceActivity.class);
-                break;
-            default:
-        }
-
-        activity.startActivity(intent);
-
-        return true;
+            switch (menuItem.getItemId()) {
+                case R.id.menu_createGame:
+                    intent = new Intent(ctx, CreateGameActivity.class);
+                    break;
+                case R.id.menu_findGame:
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable(GameListActivity.GAME_LIST_ACTIVITY_CLASS_KEY,
+                        GameListActivity.GameListType.FIND_GAME);
+                    intent = new Intent(ctx, GameListActivity.class);
+                    intent.putExtras(bundle);
+                    break;
+                case R.id.menu_dice:
+                    intent = new Intent(ctx, DiceActivity.class);
+                    break;
+                default:
+                    intent = new Intent(ctx, MyAccountActivity.class);
+            }
+            ctx.startActivity(intent);
+            return true;
+        });
     }
 
-    public static void setUsernameInMenu(NavigationView navigationView, UserProfile up) {
-
-        View headerView = navigationView.getHeaderView(0);
-        TextView username = headerView.findViewById(R.id.username_menu_textView);
-        if (up != null) {
-            username.setText(up.getUsername());
-        }
-        else {
-            username.setText("currently not logged");
-        }
+    public static void setUsernameInMenu(NavigationView navigationView, Optional<Username> username) {
+        TextView usernameTV = navigationView.getHeaderView(0).findViewById(R.id.menu_username);
+        View loggedUserPanel = navigationView.getHeaderView(0).findViewById(R.id.menu_logged_user_panel);
+        username.executeIfPresent(u -> {
+            usernameTV.setText(u.getUsername());
+            loggedUserPanel.setVisibility(View.VISIBLE);
+        }).executeIfAbsent(() -> loggedUserPanel.setVisibility(View.GONE));
     }
 
-    public static void setMenuInToolbar(AppCompatActivity activity, Toolbar toolbar){
+    public static void setMenuInToolbar(AppCompatActivity activity, Toolbar toolbar) {
         activity.setSupportActionBar(toolbar);
         ActionBar actionbar = activity.getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
