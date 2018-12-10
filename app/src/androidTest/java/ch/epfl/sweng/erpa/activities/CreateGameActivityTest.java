d5ac9a1481955f53a0ec6b0210e952e37cab7037
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-12-10 21:19:25 +0100

    Activities: Wire and rework CreateGameActivity
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- app/src/androidTest/java/ch/epfl/sweng/erpa/activities/CreateGameActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/erpa/activities/CreateGameActivityTest.java
@@ -1,7 +1,6 @@
 package ch.epfl.sweng.erpa.activities;
 
 import android.content.res.Resources;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.espresso.contrib.NavigationViewActions;
@@ -9,29 +8,25 @@ import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.Gravity;
-import android.view.View;
 
 import com.annimon.stream.Optional;
 import com.annimon.stream.Stream;
 
+import org.hamcrest.core.StringContains;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.IOException;
 
-import ch.epfl.sweng.erpa.ErpaApplication;
+import javax.inject.Inject;
+
 import ch.epfl.sweng.erpa.R;
-import ch.epfl.sweng.erpa.model.Game;
-import ch.epfl.sweng.erpa.operations.RemoteServicesProviderCoordinator;
-import ch.epfl.sweng.erpa.services.dummy.DummyRemoteServicesProvider;
-import toothpick.Scope;
-import toothpick.Toothpick;
-import toothpick.configuration.Configuration;
-import toothpick.registries.FactoryRegistryLocator;
-import toothpick.registries.MemberInjectorRegistryLocator;
+import ch.epfl.sweng.erpa.model.Username;
+import ch.epfl.sweng.erpa.operations.LoggedUserCoordinator;
+import ch.epfl.sweng.erpa.services.GCP.ServerException;
+import ch.epfl.sweng.erpa.services.UserManagementService;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -45,167 +40,132 @@ import static android.support.test.espresso.intent.matcher.IntentMatchers.hasCom
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.hasToString;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.hamcrest.core.StringStartsWith.startsWith;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
-public class CreateGameActivityTest {
+public class CreateGameActivityTest extends DependencyConfigurationAgnosticTest {
     @Rule
     public final IntentsTestRule<CreateGameActivity> intentsTestRule = new IntentsTestRule<>(
-            CreateGameActivity.class);
+        CreateGameActivity.class);
+    @Inject LoggedUserCoordinator loggedUserCoordinator;
+    @Inject UserManagementService userManagementService;
     private Resources systemResources;
-    private Game game;
-    private String uuid;
 
     @Before
-    public void setVariables() {
+    public void prepare() throws Throwable {
+        super.prepare();
         systemResources = intentsTestRule.getActivity().getResources();
-        Set<String> set = new HashSet<>();
-        game = new Game("111", "222", set, "Name", 1, 2, Game.Difficulty.NOOB, "Universe",
-                Game.OneshotOrCampaign.ONESHOT, Optional.empty(), Optional.of(30), "Description");
-
-    }
-
-    @Before
-    public void prepare() {
-        Toothpick.setConfiguration(Configuration.forDevelopment().enableReflection());
-        FactoryRegistryLocator.setRootRegistry(new ch.epfl.sweng.erpa.smoothie.FactoryRegistry());
-        MemberInjectorRegistryLocator.setRootRegistry(
-                new ch.epfl.sweng.erpa.smoothie.MemberInjectorRegistry());
-        Scope scope = Toothpick.openScope(
-                InstrumentationRegistry.getTargetContext().getApplicationContext());
-        ErpaApplication application = scope.getInstance(ErpaApplication.class);
-
-        Toothpick.reset(scope);
-        application.installModules(scope);
-        scope.getInstance(RemoteServicesProviderCoordinator.class).bindRemoteServicesProvider(
-                DummyRemoteServicesProvider.class
-        );
+        Username currentUser = registerUsername(userManagementService, "Isa");
+        registerCurrentlyLoggedUser(loggedUserCoordinator, currentUser);
     }
 
     @Test
     public void testCanParseAnySessionLength() {
-        boolean q = Stream.of(systemResources.getStringArray(R.array.session_length_array))
-                .filter(sl -> !"Undefined".equals(sl))
-                .map(CreateGameActivity::findSessionLength)
-                .allMatch(Optional::isPresent);
-        assertTrue(q);
+        assertTrue(Stream.of(systemResources.getStringArray(R.array.session_length_array))
+            .filter(sl -> !"Undefined".equals(sl))
+            .map(CreateGameActivity::findSessionLength)
+            .allMatch(Optional::isPresent));
     }
 
     @Test
     public void testCanParseAnyDifficulty() {
         //noinspection Convert2MethodRef -- Objects::nonNull was introduced in API 24
-        boolean q = Stream.of(systemResources.getStringArray(R.array.difficulties_array))
-                .map(CreateGameActivity::findDifficulty)
-                .allMatch(o -> o != null);
-        assertTrue(q);
+        assertTrue(Stream.of(systemResources.getStringArray(R.array.difficulties_array))
+            .map(CreateGameActivity::findDifficulty)
+            .allMatch(o -> o != null));
     }
 
     @Test
-    public void testGameWithPlayer() {
-        uuid = "newUuid";
-        assertTrue(game.withPlayer(uuid).getPlayersUuid().contains(uuid));
-    }
-
-    @Test
-    public void testRemovePlayer() {
-        uuid = "newUuid2";
-        game.withPlayer(uuid);
-        assertFalse(game.removePlayer(uuid).getPlayersUuid().contains(uuid));
-    }
-
-    @Test
-    public void testCanFillFormWithCorrectInputsAndNbSessions() {
+    public void testCanFillFormWithCorrectInputsAndNbSessions() throws Throwable {
         onView(withId(R.id.create_game_name_field)).perform(typeText("Game Name")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(withId(R.id.min_num_player_field)).perform(typeText("1")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(withId(R.id.max_num_player_field)).perform(typeText("5")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
 
         onView(withId(R.id.difficulty_spinner)).perform(click());
         onData(hasToString(startsWith("N"))).perform(click());
         onView(withId(R.id.difficulty_spinner)).check(
-                matches(withSpinnerText(containsString("NOOB"))));
+            matches(withSpinnerText(containsString("NOOB"))));
 
         onView(withId(R.id.universes_spinner)).perform(click());
         onData(hasToString(startsWith("O"))).perform(click());
         onView(withId(R.id.universes_spinner)).check(
-                matches(withSpinnerText(containsString("Other"))));
+            matches(withSpinnerText(containsString("Other"))));
 
         onView(withId(R.id.session_length_spinner)).perform(click());
         onData(hasToString(startsWith("5"))).perform(click());
         onView(withId(R.id.session_length_spinner)).check(
-                matches(withSpinnerText(containsString("5h"))));
+            matches(withSpinnerText(containsString("5h"))));
 
         onView(withId(R.id.campaign)).perform(click());
         onView(withId(R.id.num_session_field)).perform(typeText("2")).perform(closeSoftKeyboard());
 
         onView(withId(R.id.description_field)).perform(
-                typeText("Une petite description de partie")).perform(closeSoftKeyboard());
-        View gameForm = intentsTestRule.getActivity().findViewById(R.id.create_game_form);
-        gameForm.scrollBy(0, 500);
-        onView(ViewMatchers.withId(R.id.submit_button)).perform(ViewActions.click());
+            typeText("Une petite description de partie")).perform(closeSoftKeyboard());
+        intentsTestRule.runOnUiThread(() -> intentsTestRule.getActivity().submitGame(null));
+        intended(hasComponent(GameListActivity.class.getName()));
     }
 
     @Test
-    public void testCanFillFormWithCorrectInputsWithoutNbSessions() throws InterruptedException {
+    public void testCanFillFormWithCorrectInputsWithoutNbSessions() throws Throwable {
         onView(withId(R.id.create_game_name_field)).perform(typeText("Game Name")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(withId(R.id.min_num_player_field)).perform(typeText("1")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(withId(R.id.max_num_player_field)).perform(typeText("5")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(withId(R.id.difficulty_spinner)).perform(click());
         onData(hasToString(startsWith("N"))).perform(click());
         onView(withId(R.id.difficulty_spinner)).check(
-                matches(withSpinnerText(containsString("NOOB"))));
+            matches(withSpinnerText(containsString("NOOB"))));
         onView(withId(R.id.universes_spinner)).perform(click());
         onData(hasToString(startsWith("O"))).perform(click());
         onView(withId(R.id.universes_spinner)).check(
-                matches(withSpinnerText(containsString("Other"))));
+            matches(withSpinnerText(containsString("Other"))));
         onView(withId(R.id.session_length_spinner)).perform(click());
         onData(hasToString(startsWith("5"))).perform(click());
         onView(withId(R.id.session_length_spinner)).check(
-                matches(withSpinnerText(containsString("5h"))));
+            matches(withSpinnerText(containsString("5h"))));
         onView(withId(R.id.oneshot)).perform(click());
         onView(withId(R.id.campaign)).perform(click());
         onView(withId(R.id.description_field)).perform(
-                typeText("Une petite description de partie")).perform(closeSoftKeyboard());
-        View gameForm = intentsTestRule.getActivity().findViewById(R.id.create_game_form);
-        gameForm.scrollBy(0, 500);
-        onView(ViewMatchers.withId(R.id.submit_button)).perform(ViewActions.click());
+            typeText("Une petite description de partie")).perform(closeSoftKeyboard());
+        intentsTestRule.runOnUiThread(() -> intentsTestRule.getActivity().submitGame(null));
+        intended(hasComponent(GameListActivity.class.getName()));
     }
 
     @Test
     public void testEmptyFieldCreatesCorrectPopup() {
         onView(ViewMatchers.withId(R.id.min_num_player_field)).perform(typeText("2")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.max_num_player_field)).perform(typeText("3")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.submit_button)).perform(ViewActions.click());
-        //check if the popup is displayed
+        // Check if the popup is displayed
         onView(ViewMatchers.withText(R.string.emptyFieldMessage)).check(
-                matches(isDisplayed())).perform(ViewActions.click());
+            matches(isDisplayed())).perform(ViewActions.click());
     }
 
     @Test
     public void testEmptyCheckboxCreatesCorrectPopup() {
         onView(ViewMatchers.withId(R.id.create_game_name_field)).perform(typeText("lol")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.min_num_player_field)).perform(typeText("2")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.max_num_player_field)).perform(typeText("3")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.description_field)).perform(
-                typeText("bla bla bla")).perform(closeSoftKeyboard());
+            typeText("bla bla bla")).perform(closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.submit_button)).perform(ViewActions.click());
-        //check if the popup is displayed
+        // Check if the popup is displayed
         onView(ViewMatchers.withText(R.string.uncheckedCheckboxMessage)).check(
-                matches(isDisplayed()));
+            matches(isDisplayed()));
     }
 
     @Test
@@ -213,30 +173,30 @@ public class CreateGameActivityTest {
         onView(withId(R.id.universes_spinner)).perform(click());
         onData(hasToString(startsWith("O"))).perform(click());
         onView(withId(R.id.universes_spinner)).check(
-                matches(withSpinnerText(containsString("Other"))));
+            matches(withSpinnerText(containsString("Other"))));
         onView(withId(R.id.universe_field)).perform(typeText("KazAdrok")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
     }
 
     @Test
     public void testCreatePopUpIfMaxSmallerThanMin() {
         onView(ViewMatchers.withId(R.id.min_num_player_field)).perform(typeText("3")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.max_num_player_field)).perform(typeText("2")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.submit_button)).perform(ViewActions.click());
-        //check if the popup is displayed
+        // Check if the popup is displayed
         onView(ViewMatchers.withText(R.string.invalidPlayerNumber)).check(matches(isDisplayed()));
     }
 
     @Test
     public void test0PlayerCreatePopUp() {
         onView(ViewMatchers.withId(R.id.min_num_player_field)).perform(typeText("0")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.max_num_player_field)).perform(typeText("3")).perform(
-                closeSoftKeyboard());
+            closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.submit_button)).perform(ViewActions.click());
-        //check if the popup is displayed
+        // Check if the popup is displayed
         onView(ViewMatchers.withText(R.string.invalidPlayerNumber)).check(matches(isDisplayed()));
     }
 
@@ -260,15 +220,45 @@ public class CreateGameActivityTest {
         testClickItemMenu(R.id.menu_dice, DiceActivity.class.getName());
     }
 
+    @Test
     private void testClickItemMenu(int menuItemId, String className) {
         // Open Drawer to click on navigation.
         onView(withId(R.id.create_game_drawer_layout))
-                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
-                .perform(DrawerActions.open()); // Open Drawer
+            .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+            .perform(DrawerActions.open()); // Open Drawer
 
         // Start the screen of your activity.
         onView(withId(R.id.create_game_navigation_view))
-                .perform(NavigationViewActions.navigateTo(menuItemId));
+            .perform(NavigationViewActions.navigateTo(menuItemId));
         intended(hasComponent(className));
+    }
+
+    @Test
+    public void testPopupOnIOException() {
+        IOException e = new IOException("Connection problem");
+        intentsTestRule.getActivity().runOnUiThread(() -> intentsTestRule.getActivity().handleException(e));
+        onView(ViewMatchers.withText(StringContains.containsString("Connection problem"))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testPopupOnServerException() {
+        ServerException e = new ServerException(404, "owo");
+        intentsTestRule.getActivity().runOnUiThread(() -> intentsTestRule.getActivity().handleException(e));
+        onView(ViewMatchers.withText(StringContains.containsString("404"))).check(matches(isDisplayed()));
+        onView(ViewMatchers.withText(StringContains.containsString("Server"))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testPopupOnUnkownServErr() {
+        ServerException e = new ServerException();
+        intentsTestRule.getActivity().runOnUiThread(() -> intentsTestRule.getActivity().handleException(e));
+        onView(withText(containsString("Unknown server")));
+    }
+
+    @Test
+    public void testUnknownError() {
+        ServerException e = new ServerException();
+        intentsTestRule.getActivity().runOnUiThread(() -> intentsTestRule.getActivity().handleException(e));
+        onView(withText(containsString("Unknown")));
     }
 }
