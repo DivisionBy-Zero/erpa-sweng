0000055fa24042cb3f5ab43afade916935e22f8a
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-12-12 09:35:33 +0100

    UserProfileActivity: Add logout button
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- app/src/main/java/ch/epfl/sweng/erpa/activities/UserProfileActivity.java
+++ app/src/main/java/ch/epfl/sweng/erpa/activities/UserProfileActivity.java
@@ -1,7 +1,13 @@
 package ch.epfl.sweng.erpa.activities;
 
-import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -13,21 +19,28 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ch.epfl.sweng.erpa.R;
-import ch.epfl.sweng.erpa.operations.AsyncTaskService;
 import ch.epfl.sweng.erpa.model.UserProfile;
+import ch.epfl.sweng.erpa.operations.AsyncTaskService;
+import ch.epfl.sweng.erpa.operations.LoggedUserCoordinator;
 import ch.epfl.sweng.erpa.operations.OptionalDependencyManager;
 import ch.epfl.sweng.erpa.services.UserManagementService;
 
 import static ch.epfl.sweng.erpa.util.ActivityUtils.addNavigationMenu;
+import static ch.epfl.sweng.erpa.util.ActivityUtils.setMenuInToolbar;
 
 public class UserProfileActivity extends DependencyConfigurationAgnosticActivity {
     @Inject OptionalDependencyManager optionalDependency;
     @Inject UserManagementService ups;
+    @Inject LoggedUserCoordinator loggedUserCoordinator;
 
     @BindView(R.id.usernameTextView) TextView usernameTV;
     @BindView(R.id.experienceTextView) TextView experienceTV;
     @BindView(R.id.playerOrGMTextView) TextView playerOrGmTV;
+    @BindView(R.id.user_profile_drawer_layout) DrawerLayout myDrawerLayout;
+    @BindView(R.id.user_profile_navigation_view) NavigationView myNavigationView;
+    @BindView(R.id.user_profile_toolbar) Toolbar myToolbar;
 
     private AsyncTaskService asyncTaskService;
 
@@ -58,11 +71,19 @@ public class UserProfileActivity extends DependencyConfigurationAgnosticActivity
         asyncTaskService.run(() -> ups.getUsernameFromUserUuid(userUuid).orElseThrow(notFoundException),
             username -> usernameTV.setText("@" + username.getUsername()), this::asyncDataFetchErrorHandler);
 
-        addNavigationMenu(this, findViewById(R.id.user_profile_drawer_layout),
-            findViewById(R.id.user_profile_navigation_view), optionalDependency);
+        addNavigationMenu(this, myDrawerLayout, myNavigationView, optionalDependency);
+        setMenuInToolbar(this, myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        myToolbar.setTitle("");
     }
 
-    @SuppressLint("SetTextI18n") private void updateUserProfile(UserProfile userProfile) {
+    @OnClick(R.id.user_profile_logout_button)
+    public void logout() {
+        loggedUserCoordinator.unsetCurrentLoggedUser();
+        finish();
+    }
+
+    private void updateUserProfile(UserProfile userProfile) {
         experienceTV.setText(UserProfile.Experience.Casual.toString());
         if (userProfile.getIsGm() && userProfile.getIsPlayer())
             playerOrGmTV.setText(getString(R.string.user_profile_player_and_gm));
@@ -78,9 +99,8 @@ public class UserProfileActivity extends DependencyConfigurationAgnosticActivity
         String userUuid = getIntent().getStringExtra(UserManagementService.PROP_INTENT_USER);
         if (userUuid == null) {
             RuntimeException thrown = new IllegalArgumentException("UserUuid property not found");
-            Log.e("retrieveUuid", "Cannot show User" , thrown);
+            Log.e("retrieveUuid", "Cannot show User", thrown);
             finish();
-            throw thrown;
         }
         return userUuid;
     }
@@ -89,4 +109,5 @@ public class UserProfileActivity extends DependencyConfigurationAgnosticActivity
         Log.e("asyncErrorHandler", "Cannot populate UserProfileActivity", exc);
         finish();
     }
+
 }
