dad9b084380263d642c4a278f28f94250acd2684
Roosembert Palacios <roosembert.palacios@epfl.ch>
2018-12-17 07:06:30 +0100

    Add missing component factory to ErpaApplicationModule
    The component was still being recovered by annotation processing, but we rather
    explicitly register in a single place.
    
    Signed-off-by: Roosembert Palacios <roosembert.palacios@epfl.ch>

--- app/src/main/java/ch/epfl/sweng/erpa/modules/ErpaApplicationModule.java
+++ app/src/main/java/ch/epfl/sweng/erpa/modules/ErpaApplicationModule.java
@@ -6,9 +6,10 @@ import android.content.SharedPreferences;
 
 import ch.epfl.sweng.erpa.ErpaApplication;
 import ch.epfl.sweng.erpa.R;
+import ch.epfl.sweng.erpa.operations.DependencyConfigurationHelper;
+import ch.epfl.sweng.erpa.operations.LoggedUserCoordinator;
 import ch.epfl.sweng.erpa.operations.OptionalDependencyManager;
 import ch.epfl.sweng.erpa.operations.RemoteServicesProviderCoordinator;
-import ch.epfl.sweng.erpa.operations.LoggedUserCoordinator;
 import ch.epfl.sweng.erpa.services.GCP.GCPGameService;
 import ch.epfl.sweng.erpa.services.GCP.GCPUserManagementService;
 import ch.epfl.sweng.erpa.services.dummy.database.DummyGameService;
@@ -28,18 +29,16 @@ public class ErpaApplicationModule extends Module {
     public ErpaApplicationModule(ErpaApplication application, Scope applicationScope) {
         this.bind(Scope.class).withName(RES_APPLICATION_SCOPE).toInstance(applicationScope);
 
-        SharedPreferencesProvider preferencesProvider =
-                new SharedPreferencesProvider(application,
-                        application.getString(R.string.preference_file_key));
+        SharedPreferencesProvider preferencesProvider = new SharedPreferencesProvider(application,
+            application.getString(R.string.preference_file_key));
 
         this.bind(Application.class).toInstance(application);
         this.bind(Context.class).toInstance(application);
+        this.bind(DependencyConfigurationHelper.class).to(DependencyConfigurationHelper.class);
         this.bind(ErpaApplication.class).toInstance(application);
-        this.bind(RemoteServicesProviderCoordinator.class).to(
-                RemoteServicesProviderCoordinator.class);
-        this.bind(SharedPreferences.class).toProviderInstance(preferencesProvider);
-
         this.bind(OptionalDependencyManager.class).to(OptionalDependencyManager.class);
+        this.bind(RemoteServicesProviderCoordinator.class).to(RemoteServicesProviderCoordinator.class);
+        this.bind(SharedPreferences.class).toProviderInstance(preferencesProvider);
 
         // Dummy Remote Services Provider-related binds
         this.bind(DummyGameService.class).to(DummyGameService.class);
